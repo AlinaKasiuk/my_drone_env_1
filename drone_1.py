f2a29bf08b0dacc8c569a4564722005f01aeb2aa
Alina Kasiuk <37597014+AlinaKasiuk@users.noreply.github.com>
2020-11-04 13:54:07 +0100

    Add files via upload
    camera spot and map download testing

--- drone_1.py
+++ drone_1.py
@@ -18,18 +18,29 @@ env = gym.make('drone-v0')
 done = False
 cnt = 0
 
-observation = env.reset()
+observation, cameraspot, _= env.reset()
+print('Coordinates:')
+print(observation)
+print('Camera spot:')
+print(cameraspot)
+
+#Get relevance map
+r = np.genfromtxt('map.csv',delimiter=';',dtype='int')
+print (r)
+x,y=env.get_map(r)
+print (x,y)
 
 while not done:
     env.render()
-    print(observation)
     cnt += 1
     
     action = env.action_space.sample()
-    
+    print('Action:', action)
     observation, reward, done, _ = env.step(action)
-    
+    print('Timestep:', cnt)  
+    print('Observation:', observation )
+    print('Reward:', reward)    
     if done:
         break
     
-print ('game lasted', cnt, 'moves')
+print ("Episode finished after {} timesteps".format(cnt))
