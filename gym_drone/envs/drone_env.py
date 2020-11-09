8c53d999699310b784e709cbfe00ed8830763129
Alina Kasiuk <37597014+AlinaKasiuk@users.noreply.github.com>
2020-11-09 20:23:52 +0100

    Add files via upload
    The environment modification

--- gym_drone/envs/drone_env.py
+++ gym_drone/envs/drone_env.py
@@ -30,26 +30,32 @@ class DroneEnv(gym.Env):
      grid y index), where (0,0) is the top left of the grid ((W-1,H-1)(4,4) is max value))
      z-pos is the hight (Z is maximum flying height);
      
-        Type: Box(4)   
-        Num     Observation               Min                     Max
-        0       Current x-pos              0                    (W-1)=4
-        1       Current y-pos              0                    (H-1)=4
-        2       Current z-pos              0                    (Z-1)=4
-        3       Terrain angle              0                      2*pi 
+    Type: Box(4)
+    
+    Num |    Observation    |   Min   |    Max
+    ----|-------------------|---------|-----------
+    0   |    Current x-pos  |    0    |    W-1
+    1   |    Current y-pos  |    0    |    H-1
+    2   |    Current z-pos  |    0    |    Z-1
+    4   |    Battery level  |    0    |     1
 
-    Actions:
-        Type: Discrete(8)
-        Num   Action
-        0     None
-        1     MoveForward
-        2     MoveBackward
-        3     MoveLeft
-        4     MoveRight      
-        5     MoveUp
-        6     MoveDown    
-        7     RotateLeft
-        8     RotateRight         
-python drone_1.py
+    ### Actions:
+
+    Type: Discrete(10)
+
+    Num  |  Action
+    -----|------------------
+    0    |  Forward
+    1    |  Backward
+    2    |  Left
+    3    |  Right
+    4    |  Up      
+    5    |  Down   
+    6    |  Forward and Left
+    7    |  Forward and Right 
+    8    |  Backward and Left
+    9    |  Backward and Right  
+       
     Reward:
     ***    
 
@@ -68,29 +74,29 @@ python drone_1.py
         'video.frames_per_second': 50
     }
 
-    # TODO en el reset, escoger estado inicial aleatoriamente de las base stations
-    def __init__(self, base_stations):
-        self.bs = base_stations
+    def __init__(self):
+    
         # debug vars
     
-        self.__version__ = "2.1.1"
+        self.__version__ = "0.0.1"
     
         # Hyperparameter definition 
+        # The borders for state values (changes with downloading a map)
         self.x_min = int(0)
-        self.x_max = int(4)
+        self.x_max = int(63)
         self.y_min = int(0)
-        self.y_max = int(4)
+        self.y_max = int(63)
         self.z_min = int(0)
-        self.z_max = int(50)        
-        self.min_terr_angle = 0
-        self.max_terr_angle = 2*math.pi #terrain angle - something that is observed
+        self.z_max = int(5)        
+        self.min_battery = 0
+        self.max_battery = 100
+        
         
         self.cam_angle = 0.25*math.pi
- 
-    
+     
         self.delta_pos=1
-        self.delta_angle=0.25*math.pi
-        # ???
+        self.delta_battery=1
+        
         self.state = None #initiate state holder
         self.cameraspot=None
         
@@ -102,31 +108,31 @@ python drone_1.py
         self.grid_step_max = (self.x_max+1)*(self.y_max+1) - 1 # number of grid squares
         self.max_timestep = 2*self.grid_step_max   # Visits all grid squares twice.
     
+        """
         # Observations are (in this order): current x-pos, current y-pos, terrain angle (from horizontal axis)
-        # Let's assume that the map is of grid size 5x5. Position of the drone is represented as (grid x index,
-        # grid y index), where (0,0) is the top left of the grid ((4,4) is max value)).
+        # Let's assume that the map is of grid size 64x64. Position of the drone is represented as (grid x index,
+        # grid y index), where (0,0) is the top left of the grid ((63,63) is max value)).
+        """    
     
+        # TODO: check observation_space. Battery - ?
+        
         # Here, low is the lower limit of observation range, and high is the higher limit.
         low_ob = np.array([self.x_min,  # x-pos
                            self.y_min,  # y-pos
-                           self.min_terr_angle]) # terrain_angle_deg
+                           self.z_min]) # z-pos
         high_ob = np.array([self.x_max,  # x-pos
                             self.y_max,  # y-pos
-                            self.max_terr_angle]) # terrain_angle_deg
+                            self.z_max]) # z-pos
         self.observation_space = spaces.Box(low_ob, high_ob, dtype=np.float32)
     
-        self.action_space = spaces.Discrete(9) 
-   
-        # generate random terrain gradients/create them here
-        # import random
-        # list  = [111,222,333,444,555]
-        # print("random.choice() to select random item from list - ", random.choice(list))
-
+        self.action_space = spaces.Discrete(10) 
     
+        # TODO: Is it needed to define the default relevance map?
+        
         self.relevance_map =   [[0,0,1,0,0],
-                                [0,1,2,1,0],
-                                [1,2,3,2,1],
-                                [0,1,2,1,0],
+                                [0,1,1,1,0],
+                                [1,1,1,1,1],
+                                [0,1,1,1,0],
                                 [0,0,1,0,0]]
                            
         self.seed()
@@ -142,10 +148,9 @@ python drone_1.py
 
     def _get_cameraspot(self):
         
-        x, y, z, terr_angle = self.state
-        terr_angle=0
-        c=z
-        #*math.tan(self.cam_angle))
+        x, y, z, battery = self.state
+        c=z # *math.tan(self.cam_angle))
+        
         if  x-c<self.x_min:
             x_cam_min=self.x_min
         else:
@@ -173,8 +178,13 @@ python drone_1.py
         p4=[x_cam_max,y_cam_max]
                 
         tmp_cameraspot=[p1,p2,p3,p4]
-
-        return tmp_cameraspot    
+        
+        r=0
+        for i in range(x_cam_min,x_cam_max+1):
+            for j in range(y_cam_min,y_cam_max+1):
+                r+=self.relevance_map[i,j]
+                
+        return tmp_cameraspot,r 
     
     
     def step(self, action):
@@ -182,14 +192,8 @@ python drone_1.py
         err_msg = "%r (%s) invalid" % (action, type(action))
         assert self.action_space.contains(action), err_msg    
     
-        x, y, z, terr_angle = self.state
-        terr_angle=0
-        
-        if terr_angle>2*math.pi:
-            terr_angle=terr_angle-2*math.pi
-        elif terr_angle<0:
-            terr_angle=terr_angle+2*math.pi    
-    
+        x, y, z, battery = self.state
+            
         done = bool(
             x < self.x_min
             or x > self.x_max
@@ -197,42 +201,42 @@ python drone_1.py
             or y > self.y_max
             or z < self.z_min
             or z > self.z_max
+            or battery<0
             )  
-            
-        if done:
-            action=0
-        else:    
-            if action==1:
-                alpha=terr_angle
-                x=x+self.delta_pos*math.cos(alpha)
-                y=y+self.delta_pos*math.sin(alpha)
-            elif action==2:
-                alpha=terr_angle+math.pi
-                x=x+self.delta_pos*math.cos(alpha)    
-                y=y+self.delta_pos*math.sin(alpha)
-            elif action==3:
-                alpha=terr_angle+0.5*math.pi
-                x=x+self.delta_pos*math.cos(alpha)    
-                y=y+self.delta_pos*math.sin(alpha)        
-            elif action==4:
-                alpha=terr_angle-0.5*math.pi
-                x=x+self.delta_pos*math.cos(alpha)    
-                y=y+self.delta_pos*math.sin(alpha) 
-            elif action==5:
-                z=z+self.delta_pos    
-            elif action==6:
-                z=z-self.delta_pos
-            elif action==7:
-                terr_angle=terr_angle-self.delta_angle    
-            elif action==8:
-                terr_angle=terr_angle+self.delta_angle
+         
+        battery-=self.delta_battery
         
-        self.state=(x, y, z, terr_angle) 
-        self.cameraspot = self._get_cameraspot()
+        if action==0:
+            y+=self.delta_pos   # Forward        
+        elif action==1:
+            y-=self.delta_pos   # Backward   
+        elif action==2:
+            x-=self.delta_pos   # Left          
+        elif action==3:
+            x+=self.delta_pos   # Right   
+        elif action==4:
+            z+=self.delta_pos   # Up   
+        elif action==5:
+            z-=self.delta_pos   # Down   
+        elif action==6:
+            y+=self.delta_pos   # Forward            
+            x-=self.delta_pos   # Left             
+        elif action==7:
+            y+=self.delta_pos   # Forward            
+            x+=self.delta_pos   # Left  
+        elif action==8:
+            y-=self.delta_pos   # Backward             
+            x-=self.delta_pos   # Left             
+        elif action==9:
+            y-=self.delta_pos   # Backward           
+            x+=self.delta_pos   # Right             
         
-        self.observation=(self.state,self.cameraspot)
-        reward=self.relevance_map[int(x)][int(y)]
-      
+        self.state=(x, y, z, battery) 
+        self.cameraspot, r = self._get_cameraspot()
+        
+        self.observation=self.state
+
+        reward=r
       
         return np.array(self.observation), reward, done, {}
 
@@ -245,10 +249,12 @@ python drone_1.py
         
 
     def reset(self):
-        self.state = self.np_random.randint(low=10, high=15, size=(4,))
-        self.cameraspot = self._get_cameraspot()
+        self.state = [30,30,2,100]
+        #self.np_random.randint(low=10, high=15, size=(4,))
+        self.cameraspot, r = self._get_cameraspot()
+        
         self.steps_beyond_done = None
-        return np.array(self.state),  np.array(self.cameraspot), {} 
+        return np.array(self.state),  np.array(self.cameraspot), r 
 
 
     def render(self, mode='human'):
@@ -258,21 +264,27 @@ python drone_1.py
         world_width = self.x_max- self.x_min
         scale = screen_width/world_width
     
+        x = self.state
+        dronex = x[0] * scale 
+        droney = x[1] * scale
+        red = x[3]
+        green = 0
+        blue = 0
     
-        drone_width = 10.0
-        drone_len = 50.0
+        drone_width = 40.0
+        drone_len = 40.0
     
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             
             self.viewer = rendering.Viewer(screen_width, screen_height)
             l, r, t, b = -drone_width / 2, drone_width / 2, drone_len / 2, -drone_len / 2
-            axleoffset = drone_width / 4.0
             drone = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            drone.set_color(red, green, blue)
             self.drone_trans = rendering.Transform()
             drone.add_attr(self.drone_trans)
             self.viewer.add_geom(drone)
-            self.axle = rendering.make_circle(drone_width/2)
+            self.axle = rendering.make_circle(5)
             self.axle.add_attr(self.drone_trans)
             self.axle.set_color(.5, .5, .8)
             self.viewer.add_geom(self.axle)
@@ -281,12 +293,8 @@ python drone_1.py
             return None
 
         # Edit the pole polygon vertex
-        x = self.state
-        dronex = x[0] * scale 
-        droney = x[1] * scale         
+       
         self.drone_trans.set_translation(dronex, droney)
-        self.drone_trans.set_rotation(x[3])
-
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
 
@@ -295,6 +303,8 @@ python drone_1.py
             self.viewer.close()
             self.viewer = None
     
+        
+
         
 
 
