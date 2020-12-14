87a0a2778593a3b2bf714a78b42333409b1bfc56
Alina Kasiuk <37597014+AlinaKasiuk@users.noreply.github.com>
2020-12-14 14:41:09 +0100

    Add files via upload
    Checkerboard map added

--- create_maps.py
+++ create_maps.py
@@ -8,5 +8,18 @@ def build_1s(w, h, name="ones.csv"):
     df.to_csv(name, index=False, header=False, sep=";")
 
 
+def build_checkerboard(w, h, c, name="checkerboard.csv"):
+    map = np.zeros((w,h),dtype=int) 
+    for i in range (w):
+        for j in range (h):
+            if (i%(2*c)==0):
+                if (j%(2*c)==0):
+                    map[i:(i+c),j:(j+c)]=1
+                    map[(i+c):(i+2*c),(j+c):(j+2*c)]=1                    
+    df = pd.DataFrame(map)
+    df.to_csv(name, index=False, header=False, sep=";")
+    
+    
 if __name__ == '__main__':
-    build_1s(64, 64)
+    #build_1s(32, 32)
+    build_checkerboard(32, 32, 8)
